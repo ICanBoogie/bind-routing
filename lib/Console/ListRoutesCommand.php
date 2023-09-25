99e666d486f7d7688f0def039af7e51f572e95f8
Olivier Laviale <olivier.laviale@gmail.com>
2023-09-25 22:35:51 +0200

    Options to filter routes

--- lib/Console/ListRoutesCommand.php
+++ lib/Console/ListRoutesCommand.php
@@ -8,12 +8,17 @@ use ICanBoogie\Routing\RouteProvider;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use function array_map;
 use function implode;
 use function is_array;
+use function is_null;
+use function is_string;
+use function strtoupper;
 
 /**
  * Lists routes defined with Autoconfig.
@@ -21,6 +26,9 @@ use function is_array;
 #[AsCommand('routing:routes|routes', "List routes")]
 final class ListRoutesCommand extends Command
 {
+    private const OPT_MATCHES_URI = 'matches-uri';
+    private const OPT_MATCHES_METHOD = 'matches-method';
+
     /**
      * @param array<string, class-string<Responder>> $aliases
      *     Where _key_ is an action and _value_ a responder class.
@@ -33,11 +41,44 @@ final class ListRoutesCommand extends Command
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        $this->addOption(
+            self::OPT_MATCHES_URI,
+            'u',
+            InputOption::VALUE_OPTIONAL,
+            "Filter routes matching the URI",
+        );
+
+        $this->addOption(
+            self::OPT_MATCHES_METHOD,
+            'm',
+            InputOption::VALUE_OPTIONAL,
+            "Filter routes matching the HTTP method",
+        );
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $matches_uri = $input->getOption(self::OPT_MATCHES_URI);
+        $matches_method = $input->getOption(self::OPT_MATCHES_METHOD);
+
+        assert(is_string($matches_uri) or is_null($matches_uri));
+        assert(is_string($matches_method) or is_null($matches_method));
+
+        $matches_method = $matches_method ? RequestMethod::from(strtoupper($matches_method)) : null;
+
         $rows = [];
 
         foreach ($this->config as $route) {
+            if ($matches_uri && !$route->pattern->matches($matches_uri)) {
+                continue;
+            }
+
+            if ($matches_method && !$route->method_matches($matches_method)) {
+                continue;
+            }
+
             $rows[] = [
                 self::render_methods($route->methods),
                 $route->pattern,
@@ -45,6 +86,12 @@ final class ListRoutesCommand extends Command
                 $route->id,
                 $this->aliases[$route->action] ?? "",
             ];
+        }
+
+        if (!$rows && ($matches_uri || $matches_method)) {
+            $output->writeln("<error>The query doesn't match any route</error>");
+
+            return Command::FAILURE;
         }
 
         $table = new Table($output);
